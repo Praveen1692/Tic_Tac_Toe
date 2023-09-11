60ac9c2ff5e4d77520b3f6ed039a947bc29af7d9
Praveen Sharma <praying.sharma@gmail.com>
2023-09-11 21:27:36 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Card from './Components/Card/Card'
+import Grid from './Grid/Grid'
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+
+        <Grid numberOfCards={9} />
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
